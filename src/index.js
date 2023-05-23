0853152f4c0716a01267b386bdabce99228e8a61
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-05-22 21:37:37 -0500

    [UP] Setup Tasker factory fuction

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-document.querySelector(
-  "#content"
-).innerHTML = `<h1>Initial files are working</h1>`;
+const createTask = (id, title, description, date) => {
+  return { id, title, description, date };
+};
+
+const Imbox = [];
+const taskContainer = document.querySelector("#tasks-container");
+
+const Tasker = (() => {
+  const add = (id, title, description, date) => {
+    Imbox.push(createTask(id, title, description, date));
+  };
+
+  const render = () => {
+    let tasksHTML = "";
+    Imbox.forEach((task, index) => {
+      tasksHTML += ` 
+    <div class="task-card">
+      <div class="dividor"> 
+        <input type="checkbox">
+        <p>${task.title}</p></div>
+      <div class="dividor">
+         <p>${task.date}</p>
+        <button class="btn-fav">☆</button>
+        <button>Delete</button>
+       </div>
+     </div>`;
+    });
+    taskContainer.innerHTML = tasksHTML;
+  };
+  return { add, render };
+})();
+
+Tasker.add(1, "Learn how to code", "With The Odin Project", "05/22/23");
+Tasker.add(1, "Learn how to JS", "With The Odin Project", "05/22/23");
+Tasker.add(1, "Learn how to HTML", "With The Odin Project", "05/22/23");
+
+Tasker.render();
+
+console.log(Imbox);
+console.log(taskContainer);
